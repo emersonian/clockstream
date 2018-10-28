82bb86fb42635c302628485add53a6b3fd5ec448
Emerson Hall <e@emersonhall.com>
2018-10-28 12:43:11 -0400

    Config: Google key, Firebase database

--- consts.go
+++ consts.go
@@ -1,6 +1,9 @@
 package main
 
+const VERSION = "v0.2 (git) - e@emersonhall.com"
 const WS_URL = "ws://localhost:63968/stagedisplay"
 const WS_PORT = "63968"
 const WS_PASSWORD = "password"
-const VERSION = "v0.1 - Not ready for production use - e@emersonhall.com"
+const FIRE_URL = "https://YOUR-FIREBASE-URL.firebaseio.com"
+const GOOGLE_JSON_KEY = `{"YOUR_GOOGLE_KEY":"HERE"}`
+
